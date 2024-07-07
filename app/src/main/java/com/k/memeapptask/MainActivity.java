43b73c1548d7dd832ee50f5e0b9d4fb7cb0c36cf
khloud <kreda4262@gmail.com>
2024-07-07 16:51:27 +0300

    Task 2

---
+++ app/src/main/java/com/k/memeapptask/MainActivity.java
@@ -0,0 +1,6 @@
+package com.k.memeapptask;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+}
--- app/src/main/java/com/k/memeapptask/MainActivity.kt
+++ app/src/main/java/com/k/memeapptask/MainActivity.kt
@@ -1,20 +1,19 @@
-package com.k.memeapptask
+package com.example.constrainedtextviewapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.ImageView
+import com.k.memeapptask.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val imageView1: ImageView = findViewById(R.id.imageView)
+        imageView1.setImageResource(R.drawable.download)
+        val imageView2: ImageView = findViewById(R.id.imageView)
+        imageView2.setImageResource(R.drawable.download2)
+
     }
 }
